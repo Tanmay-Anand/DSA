fc27700c7cee73be8c304ba0ca399c5e79cfa65b
Tanmay-Anand <tanmayanand2002@gmail.com>
2025-10-28 23:29:33 +0530

    Solved some problems on arrays

---
+++ striversA2ZDSA/src/array/LongestConsecutiveSequence.java
@@ -0,0 +1,15 @@
+package array;
+
+//[102,4,100,1,101,3,2,1,1]
+//1,2,3,4: 4 and 100,101,102: 3. Therefore, 4 will be the answer
+
+public class LongestConsecutiveSequence {
+
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
+
